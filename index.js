a9ddf46599453eca4727b28963cdd0871e89aa2a
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-24 20:11:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   drivers.push(name);
 }
+
+function destructivelyPreoendDriver(name) {
+  drivers.shift(name);
+}
