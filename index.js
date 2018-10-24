5b71edabb91e7ab7a419b9867a5b3bb1dd2c2dd3
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-24 20:19:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ function prependDriver (name) {
   return [name, ...drivers];
 }
 
-function removeLastDriver() {
+function removeFirstDriver() {
   return drivers.slice(1);
 }
 
 function removeLastDriver() {
-  return drivers.slic(0, drivers.length - 1);
+  return drivers.slice(0, drivers.length - 1);
 }
